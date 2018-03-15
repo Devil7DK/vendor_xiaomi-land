37c8cc29a0d55b822bc4c7ed20140cf0016807c2
Devil7 <dineshthangavel47@gmail.com>
2018-03-15 09:05:56 +0000

    land: Add libMpeg4SwEncoder.so prebuilt

--- land/Android.mk
+++ land/Android.mk
@@ -284,4 +284,15 @@ LOCAL_MODULE_CLASS := JAVA_LIBRARIES
 LOCAL_MODULE_SUFFIX := .jar
 include $(BUILD_PREBUILT)
 
+include $(CLEAR_VARS)
+LOCAL_MODULE := libMpeg4SwEncoder
+LOCAL_MODULE_OWNER := xiaomi
+LOCAL_SRC_FILES_64 := proprietary/vendor/lib64/libMpeg4SwEncoder.so
+LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libMpeg4SwEncoder.so
+LOCAL_MULTILIB := both
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_SUFFIX := .so
+include $(BUILD_PREBUILT)
+
 endif
